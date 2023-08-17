201611d146690848329476eee29848e410aa2b37
Bolexzy <boluoyewumi79@gmail.com>
2023-08-17 21:36:10 +0100

    Fix: range error

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,19 +6,18 @@
 def rotate_2d_matrix(matrix):
     ''' Given an n x n 2D matrix, rotate it 90 degrees clockwise.
     '''
+    n = len(matrix)
 
-    for i in range(len(matrix) - 1):
+    for i in range(n):
         j = i
-        while j < len(matrix):
+        while j < n:
             temp = matrix[i][j]
             matrix[i][j] = matrix[j][i]
             matrix[j][i] = temp
             j += 1
 
-
-    for i in range(len(matrix)):
+    for i in range(n):
         p1, p2 = 0, len(matrix[i]) - 1
         temp = matrix[i][p1]
         matrix[i][p1] = matrix[i][p2]
         matrix[i][p2] = temp
-
