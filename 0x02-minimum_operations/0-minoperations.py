7c367bf419d940394708bfabeb27550aaf7f6ff6
Bolexzy <boluoyewumi79@gmail.com>
2023-08-19 18:54:54 +0100

    Add: Minimum Operations coding challenge

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''The minimum operations coding challenge.
+    Available operations:
+        copy
+        paste
+'''
+
+
+def minOperations(n):
+    ''' Given a number n, write a method that calculates the
+    fewest number of operations needed to result
+    in exactly n H characters in the file.
+    '''
+
+    min_ops = 0
+
+    if n <= 1:
+        return min_ops
+
+    for i in range(2, n + 1):
+        while n % i == 0:
+            min_ops += i
+            n /= i
+
+    return min_ops
