163190239d9b668387cf2e36a853e5ae687a3031
Bolexzy <boluoyewumi79@gmail.com>
2023-08-25 00:46:55 +0100

    Add task: 0x08. Making Change challenge

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''Making Change module.
+'''
+
+
+def makeChange(coins, total):
+    ''' Determine the fewest number of coins needed to meet a given amount
+    total,given a pile of coins of different values.
+    '''
+    if total <= 0:
+        return 0
+
+    rem = total
+    count = 0
+    coin_idx = 0
+
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            count += 1
+        else:
+            coin_idx += 1
+
+    return count
